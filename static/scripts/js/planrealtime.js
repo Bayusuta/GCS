254e1157a3144ab989dad0aa7d220858e11991b4
Ariesta Heart <ariestaheart@gmail.com>
2020-10-05 20:32:26 +0700

    Add plan.html js

---
+++ static/scripts/js/planrealtime.js
@@ -0,0 +1,170 @@
+var transform = ol.proj.getTransform('EPSG:3857', 'EPSG:4326');
+var PointerInteraction = ol.interaction.Pointer;
+
+// -- Global Variable -- //
+
+var VehicleData_List = new Map();
+var VehicleOverlay_List = new Map();
+
+var lastVehicleID = 0;
+var color_List = ["blue", "green", "red", "purple", "yellow"];
+var currentStatusDisplay = 0;
+
+var globmsg = null;
+
+// -- End of Global Variable -- //
+
+// -- Map Mouse Event -- //
+
+var Drag = (function (PointerInteraction) {
+  function Drag() {
+    PointerInteraction.call(this, {
+      handleDownEvent: handleDownEvent,
+      handleDragEvent: handleDragEvent,
+      handleMoveEvent: handleMoveEvent,
+      handleUpEvent: handleUpEvent,
+    });
+
+    this.coordinate_ = null;
+    this.cursor_ = 'pointer';
+    this.feature_ = null;
+    this.previousCursor_ = undefined;
+  }
+
+  if ( PointerInteraction ) Drag.__proto__ = PointerInteraction;
+  Drag.prototype = Object.create( PointerInteraction && PointerInteraction.prototype );
+  Drag.prototype.constructor = Drag;
+
+  return Drag;
+}(PointerInteraction));
+
+function handleDownEvent(evt) {
+  var map = evt.map;
+
+  var feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+    return feature;
+  });
+
+  if (feature) {
+    this.coordinate_ = evt.coordinate;
+    this.feature_ = feature;
+  }
+
+  return !!feature;
+}
+
+function handleDragEvent(evt) {
+  var deltaX = evt.coordinate[0] - this.coordinate_[0];
+  var deltaY = evt.coordinate[1] - this.coordinate_[1];
+
+  var geometry = this.feature_.getGeometry();
+  geometry.translate(deltaX, deltaY);
+
+  this.coordinate_[0] = evt.coordinate[0];
+  this.coordinate_[1] = evt.coordinate[1];
+}
+
+function handleMoveEvent(evt) {
+  if (this.cursor_) {
+    var coords = ol.proj.toLonLat(evt.coordinate);
+
+    var lon = coords[0];
+    var lat = coords[1];
+
+    document.getElementById('pointer-coordinate').innerHTML = "<b>Longitude</b>: " + lon + " <b>Latitude</b>: " + lat;
+
+    var map = evt.map;
+    var feature = map.forEachFeatureAtPixel(evt.pixel, function (feature) {
+      return feature;
+    });
+    var element = evt.map.getTargetElement();
+    if (feature) {
+      if (element.style.cursor != this.cursor_) {
+        this.previousCursor_ = element.style.cursor;
+        element.style.cursor = this.cursor_;
+      }
+    } else if (this.previousCursor_ !== undefined) {
+      element.style.cursor = this.previousCursor_;
+      this.previousCursor_ = undefined;
+    }
+  }
+}
+
+function handleUpEvent() {
+  this.coordinate_ = null;
+  this.feature_ = null;
+  return false;
+}
+
+// -- END OF Map Mouse Event -- //
+
+// -- THE MAP -- //
+
+var map = new ol.Map({
+  // controls: [],
+  // interactions : ol.interaction.defaults({
+  //   doubleClickZoom: false,
+  //   dragZoom: false,
+  //   keyboardZoom: false,
+  //   mouseWheelZoom: false,
+  //   pinchZoom: false,
+  // }),
+  interactions: ol.interaction.defaults().extend([new Drag()]),
+  target: 'map',
+  renderer: 'canvas', // Force the renderer to be used
+  layers: [
+    new ol.layer.Tile({
+      source: new ol.source.BingMaps({
+        key: 'AnGHr16zmRWug0WA8mJKrMg5g6W4GejzGPBdP-wQ4Gqqw-yHNqsHmYPYh1VUOR1q',
+        imagerySet: 'AerialWithLabels',
+        // imagerySet: 'Road',
+      })
+    })
+  ],
+  view: new ol.View({
+    center: ol.proj.transform([112.79758155388635, -7.2772675487336045], 'EPSG:4326', 'EPSG:3857'),
+    zoom: 18
+  })
+});
+
+// -- END OF THE MAP -- //
+
+// -- Add Mission Row -- //
+
+function addMissionRow(){
+    var commandList = `<select name="commandList" id="commandList" style="border:none; width:100%;">
+                            <option value="1">Test 1</option>
+                            <option value="2">Test 2</option>
+                            <option value="3">Test 3</option>
+                            <option value="4">Test 4</option>
+                        </select>`;
+
+    $("#tbody-mission-list").append(
+        `<tr>
+            <td>#</td>
+            <td class="id">`+commandList+`</td>
+            <td><input type="text" id="textbox-lon" style="border:none; width:100%;" disabled="true" value="104.123"/></td>
+            <td><input type="text" id="textbox-lat" style="border:none; width:100%;" disabled="true" value="53.12"/></td>
+            <td>70</td>
+            <td>0.00</td>
+            <td>387.2</td>
+            <td>
+                <button type="button" class="btn btn-default btn-sm" id="btnEdit" data-toggle="modal" data-target="#exampleModal">
+                    <i class="icon-plus"></i>
+                </button>
+                <button type="button" class="btn btn-warning btn-sm" id="btnEdit" data-toggle="modal" data-target="#exampleModal">
+                    <i class="icon-pencil"></i>
+                </button>
+                <button type="button" class="btn btn-danger btn-sm" id="btnDelete" data-toggle="modal" data-target="#exampleModal">
+                    <i class="icon-trash"></i>
+                </button>
+            </td>
+        </tr>`
+    );
+}
+
+for(var i=0; i<10; i++){
+    addMissionRow();
+}
+
+// -- End of Add Mission Row -- //
